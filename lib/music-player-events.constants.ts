0f68a9f12adc38c4a5e7506da4bffd467c129ad0
Ki Sung Bae <realgsong@hotmail.com>
2018-05-25 12:11:31 +0900

    Changed not to use getter which is regarded as function call

--- lib/music-player-events.constants.ts
+++ lib/music-player-events.constants.ts
@@ -1,15 +1,15 @@
-export class MusicPlayerEventConstants {
-  public static get ANGULAR_PLAYER_READY(): string { return 'angularPlayer:ready'; }
-  //public static get CURRENT_TRACK_DURATION(): string { return 'currentTrack:duration'; }
-  //public static get CURRENT_TRACK_POSITION(): string { return 'currentTrack:position'; }
-  public static get MUSIC_IS_PLAYING(): string { return 'music:isPlaying'; }
-  public static get MUSIC_MUTE(): string { return 'music:mute'; }
-  public static get MUSIC_REPEAT(): string { return 'music:repeat'; }
-  public static get MUSIC_VOLUME(): string { return 'music:volume'; }
-  public static get PLAYER_PLAYLIST(): string { return 'player:playlist'; }
-  public static get SOUND_MANAGER_READY(): string { return 'Sound manager ready!'; }
-  public static get TRACK_ID(): string { return 'track:id'; }
-  public static get TRACK_LOADED(): string { return 'track:loaded'; }
-  public static get TRACK_STOP(): string { return 'track:stop'; }
-  //public static get TRACK_PROGRESS(): string { return 'track:progress'; }
+export enum MusicPlayerEventConstants {
+  ANGULAR_PLAYER_READY = 'angularPlayer:ready',
+  //CURRENT_TRACK_DURATION = 'currentTrack:duration',
+  //CURRENT_TRACK_POSITION = 'currentTrack:position',
+  MUSIC_IS_PLAYING = 'music:isPlaying',
+  MUSIC_MUTE = 'music:mute',
+  MUSIC_REPEAT = 'music:repeat',
+  MUSIC_VOLUME = 'music:volume',
+  PLAYER_PLAYLIST = 'player:playlist',
+  SOUND_MANAGER_READY = 'Sound manager ready!',
+  TRACK_ID = 'track:id',
+  TRACK_LOADED = 'track:loaded',
+  TRACK_STOP = 'track:stop',
+  //TRACK_PROGRESS = 'track:progress',
 }
